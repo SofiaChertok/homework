2a4e8feb9c893f17c5d30068173c337cecb1fc57
Sofia <sofia>
2025-06-16 16:32:21 +0300

    Fixed syntax errors in 3-4.cpp and added comment explaining the bug

---
+++ DZ2/3-4.cpp
@@ -0,0 +1,12 @@
+// Bugs: Enter this program and compile it. Why does she fail? How can this be fixed?
+// Correct the error in exercise 3, then recompile it, compose it and run it.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Is there a bug here?";
+  // The bug is that the semicolon is missing after the cout statement.
+  return 0;
+}    
